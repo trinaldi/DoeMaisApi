5a820940aede4786dd37d57d4921a882e7a635a4
Tiago Rinaldi <trinaldi@gmail.com>
2025-07-21 11:31:05 -0300

    feat: add role migration to the project

---
+++ DoeMais/Migrations/20250721141833_AddRolesToUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoeMais.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRolesToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
