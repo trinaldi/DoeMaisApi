90526c636255ffb0ac96e2dcee2e056f9cf34998
Tiago Rinaldi <trinaldi@gmail.com>
2025-08-04 16:59:25 -0300

    feat: add Result<T> to address

--- DoeMais/Services/Interfaces/IAddressService.cs
+++ DoeMais/Services/Interfaces/IAddressService.cs
@@ -1,12 +1,13 @@
+using DoeMais.Common;
 using DoeMais.DTO.Address;
 
 namespace DoeMais.Services.Interfaces;
 
 public interface IAddressService
 {
-    Task<List<AddressDto>?> GetAddressesAsync(long userId);
-    Task<AddressDto?> GetAddressByIdAsync(long addressId, long userId);
-    Task<AddressDto?> CreateAddressAsync(AddressDto dto, long userId);
-    Task<AddressDto?> UpdateAddressAsync(long addressId, AddressDto dto, long userId);
-    Task<bool> DeleteAddressAsync(long addressId, long userId);
+    Task<Result<List<AddressDto>?>> GetAddressesAsync(long userId);
+    Task<Result<AddressDto>> GetAddressByIdAsync(long addressId, long userId);
+    Task<Result<AddressDto?>> CreateAddressAsync(AddressDto dto, long userId);
+    Task<Result<AddressDto?>> UpdateAddressAsync(long addressId, AddressDto dto, long userId);
+    Task<Result<bool>> DeleteAddressAsync(long addressId, long userId);
 }
