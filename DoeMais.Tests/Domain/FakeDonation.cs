a5f8b97aab531a48f6089211367c46737d2decd5
Tiago Rinaldi <trinaldi@gmail.com>
2025-08-11 10:05:00 -0300

    refactor: fix Fake entities

--- DoeMais.Tests/Domain/FakeDonation.cs
+++ DoeMais.Tests/Domain/FakeDonation.cs
@@ -9,6 +9,7 @@ public class FakeDonation
     public long UserId { get; set; }
     public long AddressId { get; set; }
     
+    public FakeAddress FakeAddress { get; set; }
     public string Title { get; set; } = "";
     public string Description { get; set; } = "";
     public int? Quantity { get; set; }
@@ -21,7 +22,7 @@ public class FakeDonation
         var faker = new Faker<FakeDonation>("pt_BR")
             .RuleFor(d => d.DonationId, f => f.IndexGlobal + 1)
             .RuleFor(d => d.UserId, _ => userId ?? 1)
-            .RuleFor(d => d.AddressId, _ => addressId ?? 1)
+            .RuleFor(d => d.AddressId, f => addressId ?? 1)
             .RuleFor(d => d.Title, f => f.Commerce.ProductName())
             .RuleFor(d => d.Description, f => f.Lorem.Sentence())
             .RuleFor(d => d.Quantity, f => f.Random.Int(1, 10))
@@ -34,4 +35,32 @@ public class FakeDonation
         
         return faker.Generate();
     } 
+    
+    public static List<FakeDonation> CreateMany(long? userId = null, long? addressId = null, int qty = 1)
+    {
+        var faker = new Faker<FakeDonation>("pt_BR")
+            .RuleFor(d => d.DonationId, f => f.IndexGlobal + 1)
+            .RuleFor(d => d.UserId, _ => userId ?? 1)
+            .RuleFor(d => d.AddressId, f => addressId ?? 1)
+            .RuleFor(d => d.Title, f => f.Commerce.ProductName())
+            .RuleFor(d => d.Description, f => f.Lorem.Sentence())
+            .RuleFor(d => d.Quantity, f => f.Random.Int(1, 10))
+            .RuleFor(d => d.Status, f => f.PickRandom<Status>())
+            .RuleFor(d => d.Images, f => new List<string>
+            {
+                f.Image.PicsumUrl(),
+                f.Image.PicsumUrl()
+            });
+        
+        return faker.Generate(qty);
+    } 
+    
+    public FakeDonation WithAddress(FakeAddress? address = null)
+    {
+        var donationId = this.DonationId;
+        address ??= FakeAddress.Create();
+
+        this.FakeAddress = address;
+        return this;
+    }
 }
